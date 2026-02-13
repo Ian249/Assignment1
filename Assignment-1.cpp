2149051383c4177f00b50c1d1ac1d1cac1298210
Ian Ashworth <i.ashworth249@gmail.com>
2026-02-13 21:23:24 +0000

    added looping question

--- Assignment-1.cpp
+++ Assignment-1.cpp
@@ -23,83 +23,111 @@ int main()
   double delta_E;
   double delta_E_joules;
   string units_answer;
+  string run_question;
+  bool repeat;
 
-  // request that the user input an atomic number value
-  cout<<"Please enter an atomic number:";
-  cin>>Z;
-
-  while(cin.fail()) 
+  while (repeat)
   {
-    cout<<"Sorry, your input was not valid, please enter a valid atomic number: "; 
-    // check variable type of input, ignore if fail
-    cin.clear(); 
-    cin.ignore(); 
+
+
+
+    // request that the user input an atomic number value
+    cout<<"Please enter an atomic number:";
     cin>>Z;
-  }
 
-  // Ask user to input initial and final quantum numbers
-  // check variable type against declaration and ignore if fail
+    while(cin.fail()) 
+    {
+      cout<<"Sorry, your input was not valid, please enter a valid atomic number: "; 
+      // check variable type of input, ignore if fail
+      cin.clear(); 
+      cin.ignore(); 
+      cin>>Z;
+    }
 
-  cout<<"Please enter an initial quantum number:";
-  cin>>ni;
+    // Ask user to input initial and final quantum numbers
+    // check variable type against declaration and ignore if fail
 
-  while(cin.fail()) 
-  {
-    cout<<"Sorry, your input was not valid, please enter a valid initial quantum number: "; 
-  
-    cin.clear(); 
-    cin.ignore(); 
-    cin>>Z;
-  }
+    cout<<"Please enter an initial quantum number:";
+    cin>>ni;
 
-  cout<<"Please enter a final quantum number:";
-  cin>>nf;
-
-  while(cin.fail()) 
-  {
-    cout<<"Sorry, your input was not valid, please enter a valid final quantum number: ";
-
-    cin.clear(); 
-    cin.ignore(); 
-    cin>>Z;
-  }
-
-  // Compute photon energy, Delta E = 13.6*(Z^2)*(1/n_f^2-1/n_i^2) eV
-  delta_E = 13.6*(pow(Z,2))*((1/pow(nf,2))-(1/pow(ni,2)));
-  delta_E_joules = delta_E * electron_volt;
- 
-
-  // ask user if they prefer an answer in joules or electron volts, provide (y/n) options, ignore if fail
-  // print answer
-  cout<<"Do you want the answer in Joules? (y/n)";
-  cin>>units_answer;
-
-  while(units_answer!="y" && units_answer!="n")
-  {
-    cout<<"Sorry, your input was not valid, please enter a valid answer: ";
-
-    units_answer.clear(); 
-    cin>>units_answer;
-  }
-
-  if (units_answer=="y")
-  {
-    cout<<"In joules, the change in energy is:\n"<<delta_E_joules<<"J";
+    while(cin.fail()) 
+    {
+      cout<<"Sorry, your input was not valid, please enter a valid initial quantum number: "; 
     
-  } else if (units_answer=="n")
-  {
-    cout<<"In electron Volts, the change in energy is:\n"<<delta_E<<"eV"<<endl;
+      cin.clear(); 
+      cin.ignore(); 
+      cin>>Z;
+    }
+
+    cout<<"Please enter a final quantum number:";
+    cin>>nf;
+
+    while(cin.fail()) 
+    {
+      cout<<"Sorry, your input was not valid, please enter a valid final quantum number: ";
+
+      cin.clear(); 
+      cin.ignore(); 
+      cin>>Z;
+    }
+
+    // Compute photon energy, Delta E = 13.6*(Z^2)*(1/n_f^2-1/n_i^2) eV
+    delta_E = 13.6*(pow(Z,2))*((1/pow(nf,2))-(1/pow(ni,2)));
+    delta_E_joules = delta_E * electron_volt;
+  
+
+    // ask user if they prefer an answer in joules or electron volts, provide (y/n) options, ignore if fail
+    // print answer
+    cout<<"Do you want the answer in Joules or electron volts? (J/eV)";
+    cin>>units_answer;
+
+    while(units_answer!="J" && units_answer!="eV")
+    {
+      cout<<"Sorry, your input was not valid, please enter a valid answer: ";
+
+      units_answer.clear(); 
+      cin>>units_answer;
+    }
+
+    if (units_answer=="y")
+    {
+      cout<<"In joules, the change in energy is:"<<delta_E_joules<<"J";
+      
+    } else if (units_answer=="n")
+    {
+      cout<<"In electron Volts, the change in energy is:"<<delta_E<<"eV"<<endl;
+
+    }
+
+
+    while(cin.fail()) 
+    {
+      cout<<"Sorry, your input was not valid, please enter a valid answer (y/n):"; 
+      cin.clear(); 
+      cin.ignore(); 
+      cin>>Z;
+    }
+
+    cout<<"do you want to run the program again? (y/n)";
+    cin>>run_question;
+    while(run_question!="J" && run_question!="eV")
+    {
+      cout<<"Sorry, your input was not valid, please enter a valid answer: ";
+
+      units_answer.clear(); 
+      cin>>run_question;
+    }
+
+    if (run_question=="y")
+    {
+      repeat = true;
+    }
+    else if (run_question=="n")
+    {
+      repeat = false;
+    }
+
 
   }
-
-
-  while(cin.fail()) 
-  {
-    cout<<"Sorry, your input was not valid, please enter a valid answer (y/n):"; 
-    cin.clear(); 
-    cin.ignore(); 
-    cin>>Z;
-  }
-
   return 0;
 }
